88a7c8414b38a15016d96c368b61d0097af0b1d2
Klay Garcia <klaywork2005@gmail.com>
2026-08-20 17:57:12 -0500

    improvements

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import numpy as np
-import cv2
+import cv2 as cv
 from collections import deque
 
 def setValues(x):
     print("")
 
-cv2.namedWindow("Color detectors")
+cv.namedWindow("Color detectors")
 
-cv2.createTrackbar("Upper Hue", "Color detectors", 153, 180, setValues)
-cv2.createTrackbar("Upper Saturation", "Color detectors", 255, 255, setValues)
-cv2.createTrackbar("Upper Value", "Color detectors", 255, 255, setValues)
+cv.createTrackbar("Upper Hue", "Color detectors", 153, 180, setValues)
+cv.createTrackbar("Upper Saturation", "Color detectors", 255, 255, setValues)
+cv.createTrackbar("Upper Value", "Color detectors", 255, 255, setValues)
 
-cv2.createTrackbar("Lower Hue", "Color detectors", 64, 180, setValues)
-cv2.createTrackbar("Lower Saturation", "Color detectors", 72, 255, setValues)
-cv2.createTrackbar("Lower Value", "Color detectors", 49, 255, setValues)
+cv.createTrackbar("Lower Hue", "Color detectors", 64, 180, setValues)
+cv.createTrackbar("Lower Saturation", "Color detectors", 72, 255, setValues)
+cv.createTrackbar("Lower Value", "Color detectors", 49, 255, setValues)
 
 bpoints = [deque(maxlen=1024)]
 gpoints = [deque(maxlen=1024)]
@@ -31,50 +31,123 @@ kernel = np.ones((5,5),np.uint8)
 colors = [(255,0,0), (0,255,0), (0,0,255), (0,255,255)]
 colorIndex = 0
 
-paintWindow = np.zeros((471,636,3), dtype=np.uint8) + 255
+paintWindow = np.zeros((480,640,3), dtype=np.uint8) + 255
 
 
-paintWindow = cv2.rectangle(paintWindow,(40,1),(140,65),(0,0,0),2)
-paintWindow = cv2.rectangle(paintWindow,(160,1),(255,65),colors[0],-1)
-paintWindow = cv2.rectangle(paintWindow,(275,1),(370,65),colors[1],-1)
-paintWindow = cv2.rectangle(paintWindow,(390,1),(455,65),colors[2],-1)
-paintWindow = cv2.rectangle(paintWindow,(505,1),(600,65),colors[3],-1)
+paintWindow = cv.rectangle(paintWindow,(40,1),(140,65),(0,0,0), -1)
+paintWindow = cv.rectangle(paintWindow,(160,1),(255,65),colors[0],-1)
+paintWindow = cv.rectangle(paintWindow,(275,1),(370,65),colors[1],-1)
+paintWindow = cv.rectangle(paintWindow,(390,1),(485,65),colors[2],-1)
+paintWindow = cv.rectangle(paintWindow,(505,1),(600,65),colors[3],-1)
 
-cv2.putText(paintWindow,"CLEAR",(49,33), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,0,0),2, cv2.LINE_AA)
+cv.putText(paintWindow,"CLEAR",(54,33), cv.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255),2, cv.LINE_AA)
 
-# cv2.namedWindow('Paint',cv2.WINDOW_AUTOSIZE)
+# cv.namedWindow('Paint',cv.WINDOW_AUTOSIZE)
 
-cap = cv2.VideoCapture(0)
+cap = cv.VideoCapture(0)
 
 while True:
     Success, frame = cap.read()
-    frame = cv2.flip(frame,1)
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    if not Success:
+        break
 
-    u_hue = cv2.getTrackbarPos("Upper Hue", "Color detectors")
-    u_saturation = cv2.getTrackbarPos("Upper Saturation", "Color detectors")
-    u_value = cv2.getTrackbarPos("Upper Value", "Color detectors")  
+    frame = cv.flip(frame,1)
+    hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
 
-    l_hue = cv2.getTrackbarPos("Lower Hue", "Color detectors")
-    l_saturation = cv2.getTrackbarPos("Lower Saturation", "Color detectors")
-    l_value = cv2.getTrackbarPos("Lower Value", "Color detectors")  
+    u_hue = cv.getTrackbarPos("Upper Hue", "Color detectors")
+    u_saturation = cv.getTrackbarPos("Upper Saturation", "Color detectors")
+    u_value = cv.getTrackbarPos("Upper Value", "Color detectors")  
+
+    l_hue = cv.getTrackbarPos("Lower Hue", "Color detectors")
+    l_saturation = cv.getTrackbarPos("Lower Saturation", "Color detectors")
+    l_value = cv.getTrackbarPos("Lower Value", "Color detectors")  
 
     Upper_hsv = np.array([u_hue, u_saturation, u_value])
     Lower_hsv = np.array([l_hue, l_saturation, l_value])
 
-    paintWindow = cv2.rectangle(paintWindow,(40,1),(140,65),(0,0,0),2)
-    frame = cv2.rectangle(frame,(160,1),(255,65),colors[0],-1)
-    frame = cv2.rectangle(frame,(275,1),(370,65),colors[1],-1)
-    frame = cv2.rectangle(frame,(390,1),(455,65),colors[2],-1)
-    frame = cv2.rectangle(frame,(505,1),(600,65),colors[3],-1)
-    cv2.putText(frame,"CLEAR",(49,33), cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),2,cv2.LINE_AA)
+    frame = cv.rectangle(frame,(40,1), (140,65), (0,0,0), -1)
+    frame = cv.rectangle(frame,(160,1),(255,65),colors[0],-1)
+    frame = cv.rectangle(frame,(275,1),(370,65),colors[1],-1)
+    frame = cv.rectangle(frame,(390,1),(485,65),colors[2],-1)
+    frame = cv.rectangle(frame,(505,1),(600,65),colors[3],-1)
 
-    cv2.imshow("White", paintWindow)
-    cv2.imshow("Cam", frame)
+    cv.putText(frame,"CLEAR",(54,33), cv.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255),2, cv.LINE_AA)
+
+    mask = cv.inRange(hsv, Lower_hsv, Upper_hsv)
+    mask = cv.erode(mask, kernel, iterations=1)
+    mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
+    mask = cv.dilate(mask, kernel, iterations=1)
+
+    cnts , z = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    center = None
+
+    if len(cnts) > 0:
+        cnt = sorted(cnts, key=cv.contourArea, reverse=True)[0]
+        ((x,y), radius) = cv.minEnclosingCircle(cnt)
+        cv.circle(frame, ((int(x),int(y))), int(radius), (0,255,255), 2)
+        M = cv.moments(cnt)
+
+        if M["m00"] != 0:
+            center = (int(M['m10']/ M['m00']), int(M['m01']/ M['m00']))
+
+        if center[1] <= 65:
+            if 40 <= center[0] <= 140:               
+                bpoints = [deque(maxlen=512)]
+                gpoints = [deque(maxlen=512)]
+                rpoints = [deque(maxlen=512)]
+                ypoints = [deque(maxlen=512)]
+
+                blue_index = 0
+                green_index = 0
+                red_index = 0
+                yellow_index = 0
+                paintWindow[67:,:,:] = 255
+            elif 160 <= center[0] <= 255:
+                colorIndex = 0 # blue
+            elif 275 <= center[0] <= 370:
+                colorIndex = 1 # green
+            elif 390 <= center[0] <= 485:
+                colorIndex = 2 # red
+            elif 505 <= center[0] <= 600:
+                colorIndex = 3 # yellow
+        else:
+                if colorIndex == 0:
+                    bpoints[blue_index].appendleft(center)
+                elif colorIndex == 1:
+                    gpoints[green_index].appendleft(center)
+                elif colorIndex == 2:
+                    rpoints[red_index].appendleft(center)
+                elif colorIndex == 3:
+                    ypoints[yellow_index].appendleft(center)
+    else:
+        bpoints.append(deque(maxlen=512))
+        blue_index += 1
+        gpoints.append(deque(maxlen=512))
+        green_index += 1
+        rpoints.append(deque(maxlen=512))
+        red_index += 1
+        ypoints.append(deque(maxlen=512))
+        yellow_index += 1
+
+    points = [bpoints,gpoints,rpoints,ypoints]
+    for i in range(len(points)):
+        for j in range(len(points[i])):
+            for k in range(1,len(points[i][j])):
+                if points[i][j][k-1] is None or points[i][j][k] is None:
+                    continue
+                cv.line(frame,points[i][j][k-1],points[i][j][k],colors[i],2)
+                cv.line(paintWindow,points[i][j][k-1],points[i][j][k],colors[i],2)
+
+    cv.imshow("Live Drawing", frame)
+    cv.imshow("Paint", paintWindow)
+    cv.imshow("Mask", mask)
 
 
-    if cv2.waitKey(1) & 0xff == ord('q'):
+
+
+    if cv.waitKey(1) & 0xff == ord('q'):
         break
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
